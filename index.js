37119e40795c83e3e21d46b7c093a1ddec0bad4b
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-23 12:54:17 -0300

    Add: Examples of variable receiving ID values

---
+++ index.js
@@ -0,0 +1,35 @@
+/*Está sendo armazenado dentro da váriavel "name" o valor da ID ("exampleInputName"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var name = document.querySelector("#exampleInputName");
+
+/*Está sendo armazenado dentro da váriavel "gender" o valor da ID ("exampleInputGenderM"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var gender = document.querySelector("#exampleInputGenderM");
+
+/*Está sendo armazenado dentro da váriavel "gender" o valor da ID ("exampleInputGenderF"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var gender = document.querySelector("#exampleInputGenderF");
+
+/*Está sendo armazenado dentro da váriavel "birth" o valor da ID ("exampleInputBirth"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var birth = document.querySelector("#exampleInputBirth");
+
+/*Está sendo armazenado dentro da váriavel "country" o valor da ID ("exampleInputCountry"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var country = document.querySelector("#exampleInputCountry");
+
+/*Está sendo armazenado dentro da váriavel "email" o valor da ID ("exampleInputEmail1"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var email = document.querySelector("#exampleInputEmail1");
+
+/*Está sendo armazenado dentro da váriavel "password" o valor da ID ("exampleInputPassword1"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var password = document.querySelector("#exampleInputPassword1");
+
+/*Está sendo armazenado dentro da váriavel "photo" o valor da ID ("exampleInputFile"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var photo = document.querySelector("#exampleInputFile");
+
+/*Está sendo armazenado dentro da váriavel "admin" o valor da ID ("#"),
+que foi procurada e selecionada pelo querySelector dentro do documento*/
+var admin = document.querySelector("#");
