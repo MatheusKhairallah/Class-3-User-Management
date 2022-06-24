bd81f715011dadb29bcc94896f25f223e82bc84f
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-24 08:34:17 -0300

    Add: Improved "gender" variable selector

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-/*Está sendo armazenado dentro da váriavel "name" o valor da ID ("exampleInputName"),
+/*Está sendo armazenado dentro da variável "name" o valor da ID ("exampleInputName"),
 que foi procurada e selecionada pelo querySelector dentro do documento*/
 var name = document.querySelector("#exampleInputName");
 
-/*Está sendo armazenado dentro da váriavel "gender" o valor da ID ("exampleInputGenderM"),
-que foi procurada e selecionada pelo querySelector dentro do documento*/
-var gender = document.querySelector("#exampleInputGenderM");
+/*Está sendo armazenado dentro da variável "gender" o ID que está marcado como 
+checked ("exampleInputGenderM ou exampleInputGenderF"), que foi procurado e selecionado
+pelo querySelector dentro do documento*/
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
 
-/*Está sendo armazenado dentro da váriavel "gender" o valor da ID ("exampleInputGenderF"),
-que foi procurada e selecionada pelo querySelector dentro do documento*/
-var gender = document.querySelector("#exampleInputGenderF");
-
-/*Está sendo armazenado dentro da váriavel "birth" o valor da ID ("exampleInputBirth"),
+/*Está sendo armazenado dentro da variável "birth" o valor da ID ("exampleInputBirth"),
 que foi procurada e selecionada pelo querySelector dentro do documento*/
 var birth = document.querySelector("#exampleInputBirth");
 
-/*Está sendo armazenado dentro da váriavel "country" o valor da ID ("exampleInputCountry"),
+/*Está sendo armazenado dentro da variável "country" o valor da ID ("exampleInputCountry"),
 que foi procurada e selecionada pelo querySelector dentro do documento*/
 var country = document.querySelector("#exampleInputCountry");
 
-/*Está sendo armazenado dentro da váriavel "email" o valor da ID ("exampleInputEmail1"),
+/*Está sendo armazenado dentro da variável "email" o valor da ID ("exampleInputEmail"),
 que foi procurada e selecionada pelo querySelector dentro do documento*/
-var email = document.querySelector("#exampleInputEmail1");
+var email = document.querySelector("#exampleInputEmail");
 
-/*Está sendo armazenado dentro da váriavel "password" o valor da ID ("exampleInputPassword1"),
+/*Está sendo armazenado dentro da variável "password" o valor da ID ("exampleInputPassword"),
 que foi procurada e selecionada pelo querySelector dentro do documento*/
-var password = document.querySelector("#exampleInputPassword1");
+var password = document.querySelector("#exampleInputPassword");
 
-/*Está sendo armazenado dentro da váriavel "photo" o valor da ID ("exampleInputFile"),
+/*Está sendo armazenado dentro da variável "photo" o valor da ID ("exampleInputFile"),
 que foi procurada e selecionada pelo querySelector dentro do documento*/
 var photo = document.querySelector("#exampleInputFile");
 
-/*Está sendo armazenado dentro da váriavel "admin" o valor da ID ("#"),
+/*Está sendo armazenado dentro da variável "admin" o valor da ID ("#exampleInputAdmin"),
 que foi procurada e selecionada pelo querySelector dentro do documento*/
-var admin = document.querySelector("#");
+var admin = document.querySelector("#exampleInputAdmin");
