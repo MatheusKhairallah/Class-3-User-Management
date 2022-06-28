f6652b5c5cbe260525f807a39cf2ab881336e8a5
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-28 08:35:23 -0300

    Add: validating if the user has registered the mandatory fields

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -19,6 +19,8 @@ class UserController {
             btn.disabled = true;
 
             let values = this.getValues();
+            
+            if (!values) return false;
 
             this.getPhoto().then(
                 (content) => {
@@ -90,8 +92,16 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add("has-error");
+                isValid = false;
+
+            }
 
             if (field.name == "gender") {
         
@@ -112,6 +122,10 @@ class UserController {
             }
         
         });
+
+        if (!isValid) {
+            return false;
+        }
     
         return new User(
             user.name, 
