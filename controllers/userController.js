eb47449987dce7dbdbd024ce7242f8b96c0d5fc4
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-30 09:33:36 -0300

    Add: Fixing localStorage save errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,6 +50,8 @@ class UserController {
 
                     user.loadFromJSON(result);
 
+                    user.save();
+
                     this.getTr(user, tr);
 
                     this.updateCount();
@@ -89,7 +91,7 @@ class UserController {
 
                     values.photo = content;
 
-                    this.insert(values);
+                    values.save();
 
                     this.addLine(values);
 
@@ -221,16 +223,6 @@ class UserController {
             this.addLine(user);
 
         })
-
-    }
-
-    insert(data) {
-
-        let users = this.getusersStorage();
-
-        users.push(data);
-
-        localStorage.setItem("users", JSON.stringify(users));
 
     }
     
